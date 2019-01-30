df7e63ff24864b988d114e9f81dde2d91de6dbac
taihsinlee <taihsinlee@kkbox.com>
2019-01-30 17:22:44 +0800

    Create FillingCharacter project

---
+++ FillingCharacters/FillingCharacters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FillingCharacters
+//
+//  Created by Tsihsin Lee on 2019/1/30.
+//  Copyright © 2019 Tsihsin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
